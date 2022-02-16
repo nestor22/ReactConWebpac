f42854b667e0668613de528fa12acc6921f19f1f
ngonzalez <nestor.ge222@gmail.com>
2022-02-15 21:40:11 -0600

    se agregan configuraciones para loaders scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports ={
     entry: './src/index.js',
@@ -12,22 +13,37 @@ module.exports ={
         extensions: ['.js', '.jsx']
     },
     module:{
-        rules:[{
-            test: /\.(js | jsx)$/,
+        rules:[ 
+        {
+            test: /\.(js|jsx)$/,
             exclude: /node_modules/,
             use: {
-                loader: 'babel-loader',
-            },
+              loader: 'babel-loader',
+            }
+          },
+          {
             test:/\.html$/,
             use: {
                 loader: 'html-loader',
             }
-        }]
+        },
+        {
+            test:/\.s[ac]ss$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        } 
+    ]
     },
     plugins:[
         new HtmlWebpackPlugin({
             template: './public/index.html',
             filename: './index.html'
+        }), 
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
         })
     ],
     devServer:{
